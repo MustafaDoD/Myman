7b44fa4506ac802f2072bc1aa43d6e67092f168b
Mustafa <61094401+MustafaDoD@users.noreply.github.com>
2024-08-02 20:18:29 +0300

    unzipgit

---
+++ config.py
@@ -0,0 +1,19 @@
+from sample_config import Config
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 24507119
+    API_HASH = "dfde852a83f236be2952259ccd42b808"
+    # the name to display in your alive message
+    ALIVE_NAME = "Mustafa"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "postgresql://u2hvw49pmt9hlyigzytq:zrFOZx1tJWWmMZEwQiCGwJiEJSEPnq@bpoqk9zyq9e9exrtbjfa-postgresql.services.clever-cloud.com:50013/bpoqk9zyq9e9exrtbjfa"
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+    STRING_SESSION = "AgGi8fAAfPAz_QTBhsMfwUueAiWymZFngLkbBJrLNlA1BXJpFrYl_YPUKyBtxGdCZmwLuPGvNPP9_kY-5OVZfNCiWii_4klg5R650jLkTR9R4WpRdyGaBn1l0DwyZR_AIl3dvhOgUroVkcwsDEoVNXBJuH307ZeA_cwa8Yrhrl_hsanRN2JP5U5pxbU-QKu0CH1fHe1zQkF2PWCThl_4Y2pj0f2s9-FCOdtCpOPzd1mOSTZEbLMCU5I69z_TWcEPTpecP3F_cYjdiMdhU6EwJkQfXNbKXA-MZZl17MAZaYeDWHofYoWOiVimbkFTi8HjjmCXp-lGRkhdJ9hR1QVlu3E8Iv0rwgAAAAA70W7UAA"
+    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
+    TG_BOT_TOKEN = "7086842410:AAEIVQTOpkzBDeiolqj2ZiLVcyafkJ2FO5Q"
+    # command handler
+    COMMAND_HAND_LER = "."
+    # sudo enter the id of sudo users userid's in that array
+    SUDO_USERS = []
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
