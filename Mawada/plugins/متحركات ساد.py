52f4e93fe86a02bab17bc7b1af74226677dca343
Mustafa <61094401+MustafaDoD@users.noreply.github.com>
2024-08-02 20:17:07 +0300

    unzipgit

---
+++ Mawada/plugins/متحركات ساد.py
@@ -0,0 +1,170 @@
+#edit  ~ @rre3l for Adyan 
+
+from telethon import events
+from Mawada.utils import admin_cmd
+from Mawada import Adyan
+from . import *
+ 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
+
+plugin_category = "extra"
+@Adyan.ar_cmd(
+    pattern="س1$",
+    command=("س1", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    rre3l = await reply_id(event)
+    if sad:
+        Adyan = f"**˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        Adyan += f"✛━━━━━━━━━━━━━✛\n"
+        Adyan += f"**الـمتحـرڪـة الأولـى **"
+        await event.client.send_file(event.chat_id, sad, caption=Adyan, reply_to=rre3l)
+
+#edit  ~ @rre3l for Adyan 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
+
+@Adyan.ar_cmd(
+    pattern="س2$",
+    command=("س2", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    leo = await reply_id(event)
+    if sad2:
+        jana = f"**˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة الـثـانيـة **"
+        await event.client.send_file(event.chat_id, sad2, caption=jana, reply_to=leo)
+
+#edit  ~ @rre3l for Adyan 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
+
+@Adyan.ar_cmd(
+    pattern="س3$",
+    command=("س3", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    sic_id = await reply_id(event)
+    if sad3:
+        jana = f"**˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة الـثـالثـة**\n"
+        await event.client.send_file(event.chat_id, sad3, caption=jana, reply_to=sic_id)
+
+#edit  ~ @rre3l for Adyan 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
+
+@Adyan.ar_cmd(
+    pattern="س4$",
+    command=("س4", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if sad4:
+        jana = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة الـرابـعـة**\n"
+        await event.client.send_file(
+            event.chat_id, sad4, caption=jana, reply_to=reply_to_id
+        )
+
+#edit  ~ @rre3l for Adyan 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
+
+@Adyan.ar_cmd(
+    pattern="س5$",
+    command=("س5", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if sad5:
+        jana = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة الـخامسـة**\n"
+        await event.client.send_file(
+            event.chat_id, sad5, caption=jana, reply_to=reply_to_id
+        )
+
+#edit  ~ @rre3l for Adyan 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
+
+@Adyan.ar_cmd(
+    pattern="س6$",
+    command=("س6", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if sad6:
+        jana = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة الـسادسـة**\n"
+        await event.client.send_file(
+            event.chat_id, sad6, caption=jana, reply_to=reply_to_id
+        )
+
+#edit  ~ @rre3l for Adyan 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
+
+@Adyan.ar_cmd(
+    pattern="س7$",
+    command=("س7", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if sad7:
+        jana = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة الـسـابعـة**\n"
+        await event.client.send_file(
+            event.chat_id, sad7, caption=jana, reply_to=reply_to_id
+        )
+      
+      
+@Adyan.ar_cmd(
+    pattern="س8$",
+    command=("س8", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if sad8:
+        jana = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة الثـامنـة**\n"
+        await event.client.send_file(
+            event.chat_id, sad8, caption=jana, reply_to=reply_to_id
+        )
+
+@Adyan.ar_cmd(
+    pattern="س9$",
+    command=("س9", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if sad9:
+        jana = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jana += f"✛━━━━━━━━━━━━━✛\n"
+        jana += f"**الـمتحـرڪـة التـاسعـة**\n"
+        await event.client.send_file(
+            event.chat_id, sad9, caption=jana, reply_to=reply_to_id
+        )
+#edit  ~ @rre3l for Adyan 
+#جميع الحقوق محفوظة لسـورس مصطفى تخـمط تبيـن فشلـك
